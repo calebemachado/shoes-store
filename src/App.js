030b535cffb9dca387f144a597b6614ec43d3bbf
Calebe Machado <calebelsm70@gmail.com>
2020-04-08 12:18:55 -0300

    add routing functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import Header from './components/Header'
+import Routes from './routes'
 
 const App = () => {
-  return <h1>Hello</h1>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  )
 }
 
 export default App
