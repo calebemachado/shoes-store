c88bcdc0b801a4831fcb9c7f2694cb7c2f7c8f0e
Calebe Machado <calebelsm70@gmail.com>
2020-04-08 19:41:07 -0300

    add toast message to handle errors to user interface

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 
 import HelmetHeader from './components/HelmetHeader'
 import Header from './components/Header'
@@ -18,6 +19,7 @@ const App = () => {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   )
