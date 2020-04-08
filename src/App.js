c09beb29e9f8a15616078916ad7c3d873f25c625
Calebe Machado <calebelsm70@gmail.com>
2020-04-08 15:31:55 -0300

    add project base structure with stylization

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
 import Routes from './routes'
+import GlobalStyle from './styles/global'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,109 @@
 import React from 'react'
 
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles'
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <ProductList>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1579006188&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1579006188&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1579006188&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1579006188&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1579006188&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1579006188&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
 
 export default Home
